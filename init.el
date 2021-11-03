fb9c50581cffe373e697acb43d304c7bc4ec295d
Rūdolfs Arvīds Kalniņš <rudolfsarvidskalnins@gmail.com>
2021-11-03 09:40:59 +0200

    updated init.el
    change backup and theme directories

--- init.el
+++ init.el
@@ -8,24 +8,64 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes '(deeper-blue))
+ '(backup-directory-alist
+   '(("" . "C:\\Users\\rudolfs.kalnins\\AppData\\Roaming\\.emacs.d\\backups")))
+ '(c-default-style
+   '((c-mode . "cc-mode")
+     (c++-mode . "cc-mode")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu")))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-buffer-indent 4)
+ '(custom-enabled-themes '(naysayer))
  '(custom-safe-themes
-   '("d2e44214a7dc0bd5b298413ed6c3ba9719f1d96794d9de3bdf7a9808902fd098" default))
+   '("cc9b288561c8b13e2eaf66b33ae869c38a4443600798e9d898a5a00456bae9e7" "583148e87f779040b5349db48b6fcad6fe9a873c6ada20487e9a1ec40d845505" "d2e44214a7dc0bd5b298413ed6c3ba9719f1d96794d9de3bdf7a9808902fd098" default))
+ '(custom-theme-directory "C:/Users/rudolfs.kalnins/.emacs.d/themes")
  '(doc-view-continuous t)
+ '(fci-rule-color "#383838")
  '(fringe-mode 0 nil (fringe))
+ '(global-auto-revert-mode t)
  '(global-visual-line-mode t)
  '(inhibit-startup-screen t)
  '(linum-format " %5i ")
+ '(nrepl-message-colors
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(safe-local-variable-values '((TeX-master . t)))
  '(scroll-bar-mode nil)
  '(setq make-backup-files)
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   '((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3")))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fixedsys" :foundry "raster" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
  
 (put 'dired-find-alternate-file 'disabled nil)
+(put 'scroll-left 'disabled nil)
+(put 'set-goal-column 'disabled nil)
